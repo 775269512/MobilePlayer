d7f211839690e5e19858b33b543425e9eed683a4
lzc <775269512@qq.com>
2019-05-08 00:31:45 +0800

    添加启动界面

---
+++ app/src/main/java/com/lzc/mobileplayer/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lzc.mobileplayer.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * author : 刘子川
+ * e-mail : 775269512@qq.com
+ * date   : 2019/5/80:09
+ * version: 1.0
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
